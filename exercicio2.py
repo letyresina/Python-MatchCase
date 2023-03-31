4deeecb3ef93e3b10c61af0f072d7ce020bbbb09
letyresina <leticiaresina@gmail.com>
2023-03-31 11:46:01 -0300

    Iniciado o exercício 2 (não finalizado)

---
+++ exercicio2.py
@@ -0,0 +1,14 @@
+'''
+    Faça um programa que receba um número, digitado pelo usuário e mostre o menu para selecionar o
+    tipo de cálculo que deve ser realizado. Exiba o resultado do cálculo efetuado.
+    Opção 1 - O dobro
+    Opção 2 - A metade
+    Opção 3 - 10% do número
+'''
+
+num = int(input("Informe um número inteiro qualquer: "))
+
+print("Escolha uma das opções a seguir:")
+print("1 - Dobro \n 2 - Metade \n 3 - 10%")
+
+opcao = int(input("Informe a opção desejada: "))
